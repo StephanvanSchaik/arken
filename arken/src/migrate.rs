30467316b8f250ebf7e71e32bb99cece39c01ea2
S.J.R. van Schaik <stephan@synkhronix.com>
2025-12-30 17:57:38 -0500

    migrate() is a simplification of migrate_to()

--- arken/src/migrate.rs
+++ arken/src/migrate.rs
@@ -23,27 +23,7 @@ pub fn migrate<P: AsRef<Path>, S: MigrationStrategy>(
     bytes: &mut BytesMut,
     path: P,
 ) -> Result<(), Error> {
-    let file = File::open(&path)?;
-
-    let size = file.metadata()?.len() as usize;
-    let size = round_up(size, MmapOptions::page_size());
-
-    if size == 0 {
-        return Ok(());
-    }
-
-    let map = unsafe { MmapOptions::new(size)?.with_file(&file, 0).map()? };
-
-    let reader = Reader::try_from(&map[..])?;
-
-    let mut writer = Writer::tempfile(Default::default())?;
-
-    S::migrate(bytes, &mut writer, &reader)?;
-
-    writer.flush()?;
-    writer.persist(path)?;
-
-    Ok(())
+    migrate_to::<_, _, S>(bytes, &path, &path)
 }
 
 pub fn migrate_to<D: AsRef<Path>, P: AsRef<Path>, S: MigrationStrategy>(
